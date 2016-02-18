5602bf37c0cee1ee302a25c821af10573e38c644
SauhardGupta <gupta.sauhard@gmail.com>
2016-02-19 00:07:32 +0530

    UPDATED THE MENU 1. added Calender Activity 2. added Threads activity 3. added Grades Activity

--- A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/Courses.java
+++ A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/Courses.java
@@ -1,10 +1,10 @@
 package spycoders.moodleplus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Courses extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -81,7 +82,7 @@ public class Courses extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.assignment) {
-            // Handle the camera action
+
         } else if (id == R.id.my_home) {
 
         } else if (id == R.id.my_courses) {
@@ -94,11 +95,13 @@ public class Courses extends AppCompatActivity
 
 
         } else if (id == R.id.threads) {
+            startActivity(new Intent(Courses.this,Threads.class));
+        } else if (id == R.id.search) {
 
         } else if (id == R.id.grades) {
-
+            startActivity(new Intent(Courses.this,Grades.class));
         } else if (id == R.id.calendar) {
-
+            startActivity(new Intent(Courses.this,My_Calendar.class));
         } else if (id == R.id.search) {
 
         } else if (id == R.id.profile) {
