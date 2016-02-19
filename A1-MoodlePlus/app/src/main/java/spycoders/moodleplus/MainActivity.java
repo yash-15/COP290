f67f583f67f0a7d3817b482996df719c3a94f4a7
yash-15 <yash.15296@gmail.com>
2016-02-20 01:42:32 +0530

    Cleaned the project

--- A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/MainActivity.java
+++ A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/MainActivity.java
@@ -2,42 +2,24 @@ package spycoders.moodleplus;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Cache;
-import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HttpClientStack;
-import com.android.volley.toolbox.HttpStack;
-import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 
 
 import org.json.JSONObject;
 
-/*
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-*/
 public class MainActivity extends AppCompatActivity {
 
     Button btn_Login,btn_Reset,btn_FP;
@@ -71,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         btn_Reset.setOnClickListener(onBut_Reset);
 
         btn_FP=(Button) findViewById(R.id.btnFP);
-        btn_FP.setOnClickListener(onBut_Fp);
+        btn_FP.setOnClickListener(onBut_FP);
 
         toast = Toast.makeText(getApplicationContext(), "Moodle Plus", Toast.LENGTH_SHORT);
         toast.show();
@@ -84,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
 private View.OnClickListener onBut_Login = new View.OnClickListener(){
         public void onClick(View v){
-            url="http://192.168.254.1:8000/default/login.json?userid="
+            url="http://10.192.47.227:8000/default/login.json?userid="
                     + txt_UserName.getText().toString()
                     + "&password="+txt_Pswd.getText().toString();
             // Request a string response from the provided URL.
@@ -125,7 +107,7 @@ private View.OnClickListener onBut_Login = new View.OnClickListener(){
         }
     };
 
-    private View.OnClickListener onBut_Fp=new View.OnClickListener() {
+    private View.OnClickListener onBut_FP =new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
@@ -140,25 +122,4 @@ private View.OnClickListener onBut_Login = new View.OnClickListener(){
     };
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/My_Calendar.java
+++ A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/My_Calendar.java
@@ -23,25 +23,4 @@ public class My_Calendar extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),
                         dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();}});
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
