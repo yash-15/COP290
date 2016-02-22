a54ac884d4675c95d576df5da0da33914b3b9a2f
PrabhuPrasadPanda <prabhupanda31@gmail.com>
2016-02-22 20:22:27 +0530

    Post Comment Functionality Added

--- A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/thread_details.java
+++ A1-MoodlePlus/app/src/main/java/spycoders/moodleplus/thread_details.java
@@ -1,9 +1,21 @@
 package spycoders.moodleplus;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -12,6 +24,8 @@ import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.net.URLEncoder;
 
 public class thread_details extends AppCompatActivity {
 
@@ -60,8 +74,9 @@ public class thread_details extends AppCompatActivity {
 
                             Queue<comment> comment1 = create_comment_queue(response);
 
-                            System.out.println(response.toString());
-                            System.out.println("Queue length: "+comment1.num_elements);
+                            LinearLayout.LayoutParams llp =
+                                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
                             while (comment1.num_elements > 0) {
 
                                 //Queue Redundancy case to be solved later
@@ -72,12 +87,83 @@ public class thread_details extends AppCompatActivity {
                                 comment_layout cl = new comment_layout(thread_details.this,
                                         temp);
 
-                                LinearLayout.LayoutParams llp =
-                                        new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
                                 ll2.addView(cl.ll, llp);
 
                             }
+                            Button btnPost=new Button(thread_details.this);
+                            btnPost.setText("Add Comment");
+                            btnPost.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                    //Help from
+                                    //http://javatechig.com/android/android-input-dialog-example
+                                    LayoutInflater inflater = LayoutInflater.from(thread_details.this);
+                                    View dialogView = inflater.inflate(R.layout.post_dialog_layout, null);
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(thread_details.this);
+                                    builder.setView(dialogView);
+                                    builder.setTitle("Post a comment to thread " + Login_act.current_thread.id);
+
+                                    final EditText txtComment = (EditText) dialogView.findViewById(R.id.txtPostDescription);
+                                    final EditText txtTitle = (EditText) dialogView.findViewById(R.id.txtPostTitle);
+
+                                    txtComment.setHint("Tester");
+                                    txtTitle.setVisibility(View.INVISIBLE);
+                                    builder.setPositiveButton("Post", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+
+                                            try {
+                                                String comment_text = URLEncoder.encode(txtComment.getText().toString(), "utf-8");
+                                                String api = "http://192.168.254.1:8000/threads/post_comment.json?" +
+                                                                "thread_id=" + Login_act.current_thread.id +
+                                                                "&description=" + comment_text;
+                                                //Source for above :
+                                                // http://stackoverflow.com/questions/573184/java-convert-string-to-valid-uri-object
+
+                                                JsonObjectRequest jsObjRequest_comment = new JsonObjectRequest
+                                                        (Request.Method.GET, api, null, new Response.Listener<JSONObject>() {
+
+                                                            @Override
+                                                            public void onResponse(JSONObject response) {
+                                                                Toast toast = Toast.makeText(getApplicationContext(),
+                                                                        "Comment Added!", Toast.LENGTH_SHORT);
+                                                                toast.show();
+                                                                Intent intent = new Intent(getBaseContext(), thread_details.class);
+                                                                finish();
+                                                                startActivity(intent);
+                                                            }
+                                                        }, new Response.ErrorListener() {
+
+                                                            @Override
+                                                            public void onErrorResponse(VolleyError error) {
+                                                                // TODO Auto-generated method stub
+
+                                                                Toast toast = Toast.makeText(getApplicationContext(),
+                                                                        "Network Error!", Toast.LENGTH_SHORT);
+                                                                toast.show();
+
+                                                            }
+                                                        });
+                                                Login_act.queue.add(jsObjRequest_comment);
+                                            }catch(Exception e){System.out.println("Failed");}
+
+                                        }
+
+                                    })
+                                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int id) {
+                                                    dialog.cancel();
+                                                }
+                                            });
+
+                                    AlertDialog input_dialog = builder.create();
+                                    input_dialog.show();
+
+                                }
+                            });
+                            llp.gravity= Gravity.RIGHT;
+                            ll2.addView(btnPost,llp);
                         }catch(Exception e){}
                     }
                 }, new Response.ErrorListener() {
